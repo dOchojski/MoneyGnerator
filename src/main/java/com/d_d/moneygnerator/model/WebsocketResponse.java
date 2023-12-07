dbff12db9950e012201e83f7832b43daeb59bea6
dziew <dziewan7@gmail.com>
2023-12-07 16:47:26 +0100

    Websockety

--- src/main/java/com/d_d/moneygnerator/model/WebsocketResponse.java
+++ src/main/java/com/d_d/moneygnerator/model/WebsocketResponse.java
@@ -1,11 +1,17 @@
 package com.d_d.moneygnerator.model;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
-public record WebsocketResponse(String id,
-                                Integer status,
-                                Map<String, Object> result,
-                                Map<String, Object> error,
-                                List<Map<String, Object>> rateLimits) {
+public record WebsocketResponse(String eventType,
+                                Long timestamp,
+                                String symbol,
+                                Long aggregateTradeId,
+                                BigDecimal price,
+                                BigDecimal quantity,
+                                BigDecimal firstTradeId,
+                                BigDecimal lastTradeId,
+                                Long tradeTimestamp,
+                                Boolean isMarketMaker) {
 }
--- src/main/java/com/d_d/moneygnerator/ws/client/BinanceWebsocketClientImpl.java
+++ src/main/java/com/d_d/moneygnerator/ws/client/BinanceWebsocketClientImpl.java
@@ -2,34 +2,43 @@ package com.d_d.moneygnerator.ws.client;
 
 import com.binance.connector.client.WebSocketStreamClient;
 import com.binance.connector.client.impl.WebSocketStreamClientImpl;
+import com.d_d.moneygnerator.model.WebsocketResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Slf4j
 @Component
 public class BinanceWebsocketClientImpl implements BinanceWebSocketClient {
-    private static final String URL = "wss://testnet.binance.vision/ws-api/v3";
     private final WebSocketStreamClient client;
+    private final ObjectMapper objectMapper;
 
     public BinanceWebsocketClientImpl() {
-        this.client = new WebSocketStreamClientImpl(URL);
+        this.client = new WebSocketStreamClientImpl();
+        this.objectMapper = new ObjectMapper();
     }
 
     @Override
     public int subscribeFor(String symbol) {
-        return client.aggTradeStream(symbol, ((event) -> log.info("Received event=[{}]", event)));
+        return client.aggTradeStream(symbol, ((event) -> log.info("Received event=[{}]", mapEventToResponse(event))));
     }
 
     @Override
     public int subscribeFor(String... symbols) {
         ArrayList<String> streams = Arrays
                 .stream(symbols)
+                .map(String::toLowerCase)
+                .map(sym -> sym + "@trade")
                 .collect(Collectors.toCollection(ArrayList::new));
-        return client.combineStreams(streams, ((event) -> log.info("Received event=[{}]", event)));
+        return client.combineStreams(streams, ((event) -> log.info("Received event=[{}]", mapEventsToResponse(event))));
     }
 
     @Override
@@ -40,5 +49,49 @@ public class BinanceWebsocketClientImpl implements BinanceWebSocketClient {
     @Override
     public void closeAllOpenConnections() {
         client.closeAllConnections();
+    }
+
+    private WebsocketResponse mapEventToResponse(String event) {
+        try {
+            Map<String, Object> parsedEvent = objectMapper.readValue(event, new TypeReference<>() {
+            });
+            return createResponse(parsedEvent);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private WebsocketResponse mapEventsToResponse(String event) {
+        try {
+            Map<String, Object> parsedEvent = objectMapper.readValue(event, new TypeReference<>() {
+            });
+            Map<String, Object> data = (Map<String, Object>) parsedEvent.get("data");
+            return createResponse(data);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private WebsocketResponse createResponse(Map<String, Object> parsedEvent) {
+        BigDecimal fValue = new BigDecimal(0);
+        if (parsedEvent.get("f") != null) {
+            fValue = new BigDecimal(String.valueOf(parsedEvent.get("f")));
+        }
+        BigDecimal lValue = new BigDecimal(0);
+        if (parsedEvent.get("l") != null) {
+            lValue = new BigDecimal(String.valueOf(parsedEvent.get("l")));
+        }
+        return new WebsocketResponse(
+                (String) parsedEvent.get("e"),
+                (Long) parsedEvent.get("E"),
+                (String) parsedEvent.get("s"),
+                (Long) parsedEvent.get("a"),
+                new BigDecimal(String.valueOf(parsedEvent.get("p"))),
+                new BigDecimal(String.valueOf(parsedEvent.get("q"))),
+                fValue,
+                lValue,
+                (Long) parsedEvent.get("T"),
+                (Boolean) parsedEvent.get("m")
+        );
     }
 }
