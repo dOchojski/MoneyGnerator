1b9de26eac21f0fb0a3d2410f1a982939081a110
dOchojski <daniel.ochojski.25.10@gmail.com>
2023-12-07 10:52:57 +0100

    Initial commit

---
+++ src/main/java/com/d_d/moneygnerator/MoneyGneratorApplication.java
@@ -0,0 +1,13 @@
+package com.d_d.moneygnerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyGneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyGneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/d_d/moneygnerator/MoneyGneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d_d.moneygnerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyGneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
