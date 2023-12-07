a69a10fb123f4cb94903e3d23dfd6e48e8c6c346
dziew <dziewan7@gmail.com>
2023-12-07 11:54:41 +0100

    Optymalizacja

--- src/main/java/com/d_d/moneygnerator/service/BinanceApiService.java
+++ src/main/java/com/d_d/moneygnerator/service/BinanceApiService.java
@@ -1,21 +1,29 @@
 package com.d_d.moneygnerator.service;
 
 import com.d_d.moneygnerator.model.PriceResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class BinanceApiService {
-
     private static final String API_BINANCE_URL = "https://api.binance.com/api/v3/ticker/price?symbol=";
-    private static final RestTemplate restTemplate = new RestTemplate();
 
+    private final RestTemplate restTemplate;
 
-    public static String getCurrentPrice(String symbol) {
+    public String getCurrentPrice(String symbol) {
         try {
-            PriceResponse response = restTemplate.getForObject(API_BINANCE_URL + symbol, PriceResponse.class);
-            return response != null ? response.price() : null;
+            return Optional.ofNullable(restTemplate.getForObject(API_BINANCE_URL + symbol, PriceResponse.class))
+                    .map(PriceResponse::price)
+                    .orElse("");
         } catch (HttpClientErrorException e) {
-            e.printStackTrace();
+            log.info("Problem podczas uderzania po symbol: {}", symbol);
             return null;
         }
     }
